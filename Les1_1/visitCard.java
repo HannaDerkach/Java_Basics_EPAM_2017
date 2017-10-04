549de2242a06e151047ceabd357ceb6cce920741
Hanna Dziarkach <anna.kazeka5493@gmail.com>
2017-10-04 16:16:20 +0300

    Hometask1

---
+++ Les1_1/visitCard.java
@@ -0,0 +1,10 @@
+package by.epam.jb.les01_4;
+
+public class visitCard {
+
+	public static void main(String[] args) {
+		System.out.println("Деркач Анна Алексеевна");
+		System.out.println("Минская область, г. Смолевичи");
+		System.out.println("+37525-957-72-73");
+	}
+}
